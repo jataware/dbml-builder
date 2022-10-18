aa82cadf14a8f15733a8ae7650f52b4bb6086010
Five Grant <5@fivegrant.com>
2022-10-18 16:57:56 -0500

    Bump `v0.1.4` to `v0.2.0`

--- dbml_builder/build.py
+++ dbml_builder/build.py
@@ -6,15 +6,16 @@ gen.py - This script handles the generated model code. The two primary
          contents has not been changed.
 """
 
+import sys
 from hashlib import sha1
 from json import dump, load
 from pydbml import PyDBML
 from omymodels import create_models
-from os import makedirs
+from os import makedirs, devnull
 from os.path import exists, join
 from pathlib import Path
 
-from dbml_builder.fix import patch
+from dbml_builder.fix import patch_ddl, patch_schema, patch_orm
 
 INFO = 'info.json'
 SCHEMAS = 'schema.py'
@@ -66,13 +67,33 @@ def generate_models(dbml_path: str, generated_dir: str) -> None:
     makedirs(generated_dir, exist_ok=True)
 
     dbml = PyDBML(Path(dbml_path))
-    schemas = create_models(patch(dbml.sql), models_type='pydantic', dump_path=join(generated_dir, SCHEMAS))
-    orm = create_models(patch(dbml.sql), models_type='sqlalchemy', dump_path=join(generated_dir, ORM))
+    ddl = patch_ddl(dbml.sql)
+
+    # Even when passing silent flags, O! My Models prints to stdout
+    visible = sys.stdout
+    sys.stdout = open(devnull, 'w')
+
+    schemas = create_models(ddl, models_type='pydantic', dump=False, exit_silent=True)
+    schema_content = patch_schema(schemas['code'])
+    schemas_handle = join(generated_dir, SCHEMAS)
+
+    orm = create_models(ddl, models_type='sqlalchemy', dump=False, exit_silent=True)
+    orm_content = patch_orm(orm['code'])
+    orm_handle = join(generated_dir, ORM)
+
+    sys.stdout = visible
+
+    with open(schemas_handle, 'w') as file:
+        file.write(schema_content)
+
+    with open(orm_handle, 'w') as file:
+        file.write(orm_content)
+
 
     info = {
       'version': get_dbml_version(dbml_path),
-      'schema_hash': sha1(schemas['code'].encode()).hexdigest(),
-      'orm_hash': sha1(orm['code'].encode()).hexdigest()
+      'schema_hash': sha1(schema_content.encode()).hexdigest(),
+      'orm_hash': sha1(orm_content.encode()).hexdigest()
     }
 
     with open(join(generated_dir, INFO), 'w') as file:
